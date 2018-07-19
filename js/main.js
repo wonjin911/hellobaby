da350f0e1504b919ab0e8ba51e9d42ba7a4a7c39
Wonjin Kim <wonjin@Wonjinui-MacBook-Pro.local>
2018-07-20 03:25:58 +0900

    dialog random msg

--- js/main.js
+++ js/main.js
@@ -23,26 +23,31 @@ var baby = new Path.Circle({
 	radius: 50,
 	fillColor: 'black',
 	strokeColor: 'white'
-});
-//baby.fullySelected = true;
+});//baby.fullySelected = true;
 
 //var bottom2 = view.center + new Point(20, 120);
 //baby.insert(0, bottom2);
 baby.smooth();
 
-console.log(baby.position);
-var rectangle = new Rectangle(baby.position, baby.position + new Point(150, -75));
+// Define Dialog and text
+var rectangle = new Rectangle(baby.position + new Point(0, -25), baby.position + new Point(150, -100));
 var cornerSize = new Size(20, 20);
 var dialog = new Path.RoundRectangle(rectangle, cornerSize);
-dialog.fillColor = 'black';
+dialog.fillColor = 'white';
+dialog.strokeColor = 'black';
 
-var text = new PointText(dialog.position);
-text.content = '엄마 안녕!';
-text.fillColor = 'white';
+var text_list = ['엄마 안녕!', '졸려요', '사랑해요', '엄마 힘내세요!', '고마워요 엄마'];
+console.log(dialog.bounds);
+var text = new PointText(dialog.bounds.leftCenter + new Point(30, 0));
+var rnum = Math.floor(Math.random() * (text_list.length));
+text.content = text_list[rnum];
+text.fillColor = 'black';
 
 baby.onMouseDown = function(event) {
 	this.fillColor = 'white';
 	this.strokeColor = 'black';
+	rnum = Math.floor(Math.random() * (text_list.length));
+	text.content = text_list[rnum];
 }
 baby.onMouseUp = function(event) {
 	this.fillColor = 'black';
@@ -55,7 +60,7 @@ function onFrame(event) {
 	// the position of the placed symbols:
 	for (var i = 0; i < count; i++) {
 		var item = group.children[i] //project.activeLayer.children[i];
-		
+
 		// Move the item 1/20th of its width to the right. This way
 		// larger circles move faster than smaller circles:
 		item.position.x += item.bounds.width / 50;
@@ -66,7 +71,7 @@ function onFrame(event) {
 		if (item.bounds.left > view.size.width) {
 			item.position.x = -item.bounds.width;
 		}
-		if (item.bounds.up > view.size.height) {
+		if (item.bounds.top > view.size.height) {
 			item.position.y = -item.bounds.height;
 		}
 	}
